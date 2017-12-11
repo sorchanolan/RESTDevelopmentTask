f96694572511b20dff2f107b2a08fef8d85cdb44
sorchanolan <32638110+sorchanolan@users.noreply.github.com>
2017-12-11 19:02:27 +0000

    Manager sends out commits when requested until none left

--- manager.py
+++ manager.py
@@ -8,14 +8,28 @@ from cyclomatic_complexity import cyclo_complex
 NUM_WORKERS = 0
 REPO_URL = "https://github.com/sorchanolan/RESTDevelopmentTask"
 commits_list = []
+commits_index = 0
 repo = None
+finished = False
 
 app = Flask(__name__)
 api = Api(app)
 
 class Manager(Resource):
+
+	def get(self): 
+		global commits_list, commits_index, finished
+
+		if commits_index < len(commits_list):
+			commit = commits_list[commits_index]
+			commits_index += 1
+		else:
+			commit = None
+			finished = True
+		return {"commit": commit, "finished": finished}
+
+
 	def post(self):  print "POST request"
-	def get(self): return {"repo":repo}
 
 class AddWorker(Resource):
 	def get(self): 
@@ -43,5 +57,5 @@ if __name__ == '__main__':
 	for commit in repo.iter_commits():
 		commits_list.append(str(commit))
 	print 'There are {0} commits'.format(len(commits_list))
-	
+
 	app.run(host='127.0.0.1', port=5000, debug=False)
--- worker.py
+++ worker.py
@@ -2,20 +2,43 @@ import requests
 from flask_restful import Resource, Api
 from flask import Flask, request
 
+WORKER_ID = ""
 GET_WORK_URL = "http://127.0.0.1:5000"
 ADD_WORKER_URL = "http://127.0.0.1:5000/add_worker"
+REPO_URL = "https://github.com/sorchanolan/RESTDevelopmentTask"
+commits_list = []
+repo = None
+count = 0
 
 class Worker(object):
 	def __init__(self):
-		self.worker_id = register_worker()
+		add_worker()
+		self.running = True
 
+	def steal_work(self):
+		global count 
 
-def register_worker():
-    response = requests.get(ADD_WORKER_URL)
-    worker_id = response.json()['new_worker']
-    print 'Worker id assigned: {0}'.format(worker_id)
-    return worker_id
+		while(self.running):
+			response = requests.get(GET_WORK_URL, json={"worker_id": WORKER_ID})
+			if response.json()['finished'] is True:
+				self.running = False
+				break
+			else:
+				commit = response.json()['commit']
+				count += 1 
+				self.execute_task(commit)
+
+	def execute_task(self, commit):
+		print "{0}".format(count)
+
+def add_worker():
+	global WORKER_ID
+	response = requests.get(ADD_WORKER_URL)
+	worker_id = response.json()['new_worker']
+	print 'Worker id assigned: {0}'.format(worker_id)
+	WORKER_ID = str(worker_id)
 
 if __name__ == '__main__':
     worker = Worker()
+    worker.steal_work() 
 
